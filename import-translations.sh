da9a3c94e3b50a7554e027c5caa43d281e1a8640
Sukhbir Singh <sukhbir@torproject.org>
2018-02-01 10:26:50 -0500

    Update translations
    - Update import-translations.sh (tor-launcher script)
    - Update and add existing translations from Transifex to TorBirdy

--- import-translations.sh
+++ import-translations.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+# Script borrowed from tor-launcher and modified for TorBirdy.
 
-# Script to fetch and update translations. Modified from tor-launcher.
-
-BUNDLE_LOCALES="nl lv de uk ko pl fr nb he el sv sl pt cs it pt-BR  \
-                es id ro sr da ca pa en-GB hu sk ru ar eu tr"
+TRANSLATION_BRANCHES="
+torbirdy
+"
 
 if [ -d translation ];
 then
@@ -15,12 +15,18 @@ else
 fi
 
 cd translation
-for i in $BUNDLE_LOCALES
+for branch in ${TRANSLATION_BRANCHES}
 do
-  UL="`echo $i|tr - _`"
-
-  git checkout torbirdy
-  git merge origin/torbirdy
-  cp $UL/torbirdy.dtd ../chrome/locale/$i/torbirdy.dtd
-  cp $UL/torbirdy.properties ../chrome/locale/$i/torbirdy.properties
+  git checkout ${branch}
+  git merge origin/${branch}
+  for locale in *
+  do
+    if [ ! -d "${locale}" ]
+    then
+      continue
+    fi
+    target="../chrome/locale/$(echo "${locale}" | tr _ -)"
+    mkdir -p "${target}"
+    cp -f "${locale}"/* "${target}"/
+  done
 done
